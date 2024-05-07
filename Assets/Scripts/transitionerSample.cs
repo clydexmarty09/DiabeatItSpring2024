de0f61331f67c918686bf93c552e42f7fbbf6cb9
clydexmarty09 <cmarty@ucsd.edu>
2024-05-07 14:50:33 -0700

    did more stuff

---
+++ Assets/Scripts/transitionerSample.cs
@@ -0,0 +1,40 @@
+//this is an example file 
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using UnityEngine.SceneManagement; 
+
+public class SceneLoader : MonoBehaviour {
+public SceneTransition transitioner; // SceneTransition already defined
+public string sceneNameToLoad; 
+
+	void start() {
+		// implement this ; ie: transitioner.LoadScene(//scene name here)
+	}
+
+
+} // end class 
+
+/*
+using System.Collections; 
+using System.Collenctions.Generic; 
+using UnityEngine; 
+using UnityEngine.SceneManagement; 
+
+public class SceneLoader: MonoBehaviour 
+{
+	public SceneTransition transitioner; 
+	public string[] scenes; 
+	public int currentSceneIndex = 0; 
+
+	public void LoadNextScene()
+	{
+		if (currentSceneIndex < scenes.length) {
+			transitioner.LoadScene(scenes	
+		}
+	
+	}
+
+
+}
+*/
